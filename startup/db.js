dc73ef6ef2e2d20fbe27286084db292bb617d9e2
unknown <mohamedashrafma05@gmail.com>
2020-06-16 03:10:18 +0200

    new

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 module.exports = function () {
-  mongoose.connect(process.env.mongodbUrl, {useCreateIndex: true,
+  mongoose.connect('mongodb://localhost/Task', {useCreateIndex: true,
   useNewUrlParser: true})
     .then(() => console.log("Connetion To MongoDB ......."))
     .catch((err) => console.error('error When Connecting to MongoDB...',err))
